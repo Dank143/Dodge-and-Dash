967c3dfdb111c7326b50d3d502029afca3e9c9e9
Dang Nguyen <Dank143@users.noreply.github.com>
2026-01-12 23:13:21 +1100

    Upload from local pc

---
+++ models/speedometer.rb
@@ -0,0 +1,24 @@
+# This class contains the speedometer properties.
+class Speedometer
+  MAX_ANGLE = 75
+  MIN_ANGLE = -90
+
+  def initialize
+    @speedometer = Gosu::Image.new(Path::IMAGES + 'speedometer.png')
+    @speedometer_pointer =
+      Gosu::Image.new(Path::IMAGES + 'speedometer-pointer.png')
+    @x = WIDTH - 80
+    @y = HEIGHT - 80
+  end
+
+  def angle(player)
+    old_range = player.speed_limit - player.speed_minumum
+    new_range = MAX_ANGLE - MIN_ANGLE
+    ((player.speed - player.speed_minumum) * new_range) / old_range + MIN_ANGLE
+  end
+
+  def draw(player)
+    @speedometer.draw_rot(@x, @y, ZOrder::ELEMENT, 0.0)
+    @speedometer_pointer.draw_rot(@x, @y, ZOrder::ELEMENT, angle(player))
+  end
+end
